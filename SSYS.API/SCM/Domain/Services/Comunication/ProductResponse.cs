c0e18cbf0e4ee5e9a5cde55968e974832c6af871
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-30 20:37:58 -0500

    Added product response

---
+++ SSYS.API/SCM/Domain/Services/Comunication/ProductResponse.cs
@@ -0,0 +1,15 @@
+﻿using SSYS.API.SCM.Domain.Models;
+using SSYS.API.Shared.Domain.Services.Comunication;
+
+namespace SSYS.API.SCM.Domain.Services.Comunication;
+
+public class ProductResponse: BaseResponse<Product>
+{
+    public ProductResponse(Product resource) : base(resource)
+    {
+    }
+
+    public ProductResponse(string message) : base(message)
+    {
+    }
+}
