8ea639312e51bd052ca15fae22878eb2c5919790
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-30 20:36:44 -0500

    product class added

---
+++ SSYS.API/SCM/Domain/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using SSYS.API.Shared.Domain.Model;
+
+namespace SSYS.API.SCM.Domain.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Amount { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+}
