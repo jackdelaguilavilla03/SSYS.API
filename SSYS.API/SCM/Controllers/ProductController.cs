71948896943ecd3d139c48b016171b063ad56fea
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-30 21:06:22 -0500

    ProductController added terminado

--- SSYS.API/SCM/Controllers/ProductController.cs
+++ SSYS.API/SCM/Controllers/ProductController.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SSYS.API.SCM.Domain.Models;
+using SSYS.API.SCM.Domain.Services;
 
 namespace SSYS.API.SCM.Controllers;
 
-
-public class ProductController
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class ProductController:ControllerBase
 {
+   private readonly IProductService _productService;
+
+   public ProductController(IProductService productService)
+   {
+      _productService = productService;
+   }
    
+   [HttpGet]
+   public async Task<IEnumerable<Product>> GetAllAsync()
+   {
+      var  product= await _productService.ListAsync();
+      return product;
+   }
 }
