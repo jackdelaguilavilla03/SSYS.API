12f4dcb749c87ff8afe0e1f8f801bcc5a851e7bb
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-30 20:39:13 -0500

    ProductSevice class added

---
+++ SSYS.API/SCM/Domain/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using SSYS.API.SCM.Domain.Models;
+using SSYS.API.SCM.Domain.Services.Comunication;
+
+namespace SSYS.API.SCM.Domain.Services;
+
+public interface IProductService
+{
+    Task<IEnumerable<Product>> ListAsync();
+    Task<IEnumerable<Product>> ListByCategoryIdAsync(int product);
+    Task<ProductResponse> SaveAsync(Product product);
+    Task<ProductResponse> UpdateAsync(int productId, Product product);
+    Task<ProductResponse> DeleteAsync(int productId);
+}
