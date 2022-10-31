25a3c90072e97aff0010ac1b3dfa41738ec53113
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-10-31 12:24:38 -0500

    SaveProduct added

---
+++ SSYS.API/SCM/Resources/ProductResource.cs
@@ -0,0 +1,10 @@
+﻿namespace SSYS.API.SCM.Resources;
+
+public class ProductResource
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Amount { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+}
---
+++ SSYS.API/SCM/Resources/SaveProductResource.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SSYS.API.SCM.Resources;
+
+public class SaveProductResource
+{
+   [Required]
+   public string Title { get; set; }
+   [Required]
+   public int Amount { get; set; }
+   [Required]
+   public int Price { get; set; } 
+}
