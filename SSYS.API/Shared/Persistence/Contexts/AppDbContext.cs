3b6cbb0643c87f04f2caaeff36e22c4728d351d3
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-02 17:46:54 -0500

    Reglas de mapeo: Product

--- SSYS.API/Shared/Persistence/Contexts/AppDbContext.cs
+++ SSYS.API/Shared/Persistence/Contexts/AppDbContext.cs
@@ -1,6 +1,7 @@
 using System.Data.Entity;
 using Microsoft.EntityFrameworkCore;
 using SSYS.API.IAM.Domain.Models;
+using SSYS.API.SCM.Domain.Models;
 using DbContext = Microsoft.EntityFrameworkCore.DbContext;
 
 namespace SSYS.API.Shared.Persistence.Contexts;
@@ -13,6 +14,8 @@ public class AppDbContext : DbContext
     public Microsoft.EntityFrameworkCore.DbSet<MainUser> MainUsers { get; set; }
     public Microsoft.EntityFrameworkCore.DbSet<User> Users { get; set; }
     
+    public System.Data.Entity.DbSet<Product> Products { get; set; }//1
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         // Relationships
@@ -21,19 +24,27 @@ public class AppDbContext : DbContext
             .WithOne(u => u.MainUser)
             .HasForeignKey(u => u.MainUserId)
             .OnDelete(DeleteBehavior.Cascade);
-        
+
         //Main User
         builder.Entity<MainUser>().ToTable("MainUsers");
         builder.Entity<MainUser>().HasKey(p => p.Id);
         builder.Entity<MainUser>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<MainUser>().Property(p => p.Username).IsRequired().HasMaxLength(100);
-        
+
         //User
         builder.Entity<User>().ToTable("Users");
         builder.Entity<User>().HasKey(p => p.Id);
         builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<User>().Property(p => p.Username).IsRequired().HasMaxLength(100);
+
+        //relationships-> con category
+        builder.Entity<Product>().ToTable("Products");
+        builder.Entity<Product>().HasKey(p => p.Id);
+        builder.Entity<Product>().Property(p => p.Title).IsRequired();
+        builder.Entity<Product>().Property(p => p.Id).IsRequired();
+        builder.Entity<Product>().Property(p => p.Amount);
+        builder.Entity<Product>().Property(p => p.Price).IsRequired();
+        builder.Entity<Product>().Property(p => p.Date).IsRequired();
     }
-    
-    
+
 }
