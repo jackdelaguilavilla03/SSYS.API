531c6ff6220135157d0fd5c1be3dc1ab6214bc05
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-03 11:41:09 -0500

    Create CategoryResource.cs

---
+++ SSYS.API/SCM/Resources/CategoryResource.cs
@@ -0,0 +1,8 @@
+﻿namespace SSYS.API.SCM.Resources;
+
+public class CategoryResource
+{
+   public int Id { get; set; }
+   public string Title { get; set; }
+   public string Description { get; set; }
+}
