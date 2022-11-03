0a722dced27a6dcdf60e6e30dd9826dc8307e834
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-03 10:57:15 -0500

    Create ICategoryRepository.cs

---
+++ SSYS.API/SCM/Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using SSYS.API.SCM.Domain.Models;
+
+namespace SSYS.API.SCM.Domain.Repositories;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> ListAsync();
+    Task AddAsync(Category category);
+    Task<Category> FindByIdAsync(int id);
+    void Update(Category category);
+    void Remove(Category category);
+    Task<IEnumerable<Category>> FindByCategoryTitleAsync(string title);
+}
