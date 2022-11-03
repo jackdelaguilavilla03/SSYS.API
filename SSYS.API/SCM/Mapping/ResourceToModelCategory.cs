01400462c864f0b15a466db4e69e93aa81f49b07
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-03 13:20:00 -0500

    Create ResourceToModelCategory.cs

---
+++ SSYS.API/SCM/Mapping/ResourceToModelCategory.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SSYS.API.SCM.Domain.Models;
+using SSYS.API.SCM.Resources;
+
+namespace SSYS.API.SCM.Mapping;
+
+public class ResourceToModelCategory: Profile
+{
+    protected ResourceToModelCategory()
+    {
+        CreateMap<SaveCategoryResource, Category>();
+    }
+}
