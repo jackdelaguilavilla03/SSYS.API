3dc2151740ce2d22f2864a7374aecc82b4260991
GiulianoMendez <u201824528@upc.edu.pe>
2022-11-06 13:21:21 -0500

    feat(suppliers): implemented supplier service

---
+++ SSYS.API/SCM/Domain/Models/Supplier.cs
@@ -0,0 +1,12 @@
+﻿namespace SSYS.API.SCM.Domain.Models;
+
+public class Supplier
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Ruc { get; set; }
+    public string Address { get; set; }
+    public int Phone { get; set; }
+    public string Email { get; set; }
+
+}
---
+++ SSYS.API/SCM/Domain/Services/Comunication/SupplierResponse.cs
@@ -0,0 +1,15 @@
+﻿using SSYS.API.SCM.Domain.Models;
+using SSYS.API.Shared.Domain.Services.Comunication;
+
+namespace SSYS.API.SCM.Domain.Services.Comunication;
+
+public class SupplierResponse: BaseResponse<Supplier>
+{
+    public SupplierResponse(Supplier resource) : base(resource)
+    {
+    }
+
+    public SupplierResponse(string message) : base(message)
+    {
+    }
+}
