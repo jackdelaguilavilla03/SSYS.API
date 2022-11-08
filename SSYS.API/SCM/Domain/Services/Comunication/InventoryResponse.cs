69fabcc048cccd44183e4d947914326b9b43eb48
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-08 15:58:57 -0500

    Added Interface Service Inventory

---
+++ SSYS.API/SCM/Domain/Services/Comunication/InventoryResponse.cs
@@ -0,0 +1,15 @@
+﻿using SSYS.API.SCM.Domain.Models;
+using SSYS.API.Shared.Domain.Services.Comunication;
+
+namespace SSYS.API.SCM.Domain.Services.Comunication;
+
+public class InventoryResponse: BaseResponse<Inventory>
+{
+    public InventoryResponse(Inventory resource) : base(resource)
+    {
+    }
+
+    public InventoryResponse(string message) : base(message)
+    {
+    }
+}
---
+++ SSYS.API/SCM/Domain/Services/IInventoryService.cs
@@ -0,0 +1,12 @@
+﻿using SSYS.API.SCM.Domain.Models;
+
+namespace SSYS.API.SCM.Domain.Services;
+
+public class IInventoryService
+{
+   Task<IEnumerable<Inventory>> ListAsync();
+   Task<IEnumerable<Inventory>> ListByCategoryIdAsync(int IdCategory);
+   Task<IEnumerable<Inventory>> ListByProductIdAsync(int IdProduct);
+   Task<IEnumerable<Inventory>> ListByCategoryTitleAsync(string TitleCategory);
+   Task<IEnumerable<Inventory>> ListByProductTitleAsync(string TitleCategory);
+}
