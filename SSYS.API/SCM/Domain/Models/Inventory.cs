6d3911827131b715365aaaad37efbd94bc1ae7dd
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-08 15:50:53 -0500

    Add class Inventory

---
+++ SSYS.API/SCM/Domain/Models/Inventory.cs
@@ -0,0 +1,10 @@
+﻿namespace SSYS.API.SCM.Domain.Models;
+
+public class Inventory
+{
+   public int Id { get; set; } 
+   public string ProductTitle { get; set; }
+   public string ProductCategory { get; set; }
+   public int IdProduct { get; set; }
+   public int IdCategory { get; set; }
+}
--- SSYS.API/SCM/Domain/Services/IProductService.cs
+++ SSYS.API/SCM/Domain/Services/IProductService.cs
@@ -8,7 +8,7 @@ public interface IProductService
     Task<IEnumerable<Product>> ListAsync();
     Task<IEnumerable<Product>> ListByCategoryIdAsync(int product);
     Task<ProductResponse> SaveAsync(Product product);
-    Task<ProductResponse> UpdateAsync(int productId, Product product);
+    Task<ProductResponse> UpdateAsync(int storageId, Product product);
     Task<ProductResponse> DeleteAsync(int productId);
     Task<IEnumerable<Product>> FindByProductPriceAsync(int price);
 }
