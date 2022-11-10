ec20607028a61df014373c0e32f9fd41be1d8ca7
JuanPabloPT2001 <u201915206@gmail.com>
2022-11-10 15:01:30 -0500

    feat(salesOrder): controller get salesOrder method implemented.

---
+++ SSYS.API/CRM/Interfaces/Rest/Controllers/SalesOrderController.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Mime;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SSYS.API.CRM.Domain.Models;
+using SSYS.API.CRM.Domain.Services;
+using SSYS.API.CRM.Resources;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace SSYS.API.CRM.Interfaces.Rest.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Create, read, update and delete SalesOrder")]
+public class SalesOrderController
+{
+    private readonly ISaleOrderService _saleOrderService;
+    private readonly IMapper _mapper;
+
+
+    public SalesOrderController(ISaleOrderService saleOrderService, IMapper mapper)
+    {
+        _saleOrderService = saleOrderService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<SaleOrderResource>> GetAllAsync()
+    {
+        var salesOrder = await _saleOrderService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<SaleOrder>, IEnumerable<SaleOrderResource>>(salesOrder);
+        return resources;
+    }
+    
+}
---
+++ SSYS.API/CRM/Resources/SaleOrderResource.cs
@@ -0,0 +1,14 @@
+﻿using SSYS.API.CRM.Domain.Models;
+using SSYS.API.SCM.Domain.Models;
+
+namespace SSYS.API.CRM.Resources;
+
+public class SaleOrderResource
+{
+    public int Id { get; set; }
+    public MethodOfPayment MethodOfPayment { get; set; }
+    public Customer Customer { get; set; }
+    public Category Category { get; set; }
+    public Product Product { get; set; }
+    public int Amount { get; set; }
+}
