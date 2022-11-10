dd1c84a4f19a25c7611b3fbf35b9c8d2d53c9bae
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-10 14:33:07 -0500

    Add function

--- SSYS.API/SCM/Domain/Services/IInventoryService.cs
+++ SSYS.API/SCM/Domain/Services/IInventoryService.cs
@@ -6,6 +6,7 @@ namespace SSYS.API.SCM.Domain.Services;
 public interface IInventoryService
 {
    Task<IEnumerable<Inventory>> ListAsync();
+   Task<IEnumerable<Inventory>> ListByIdAsync(int IdInventory);
    Task<IEnumerable<Inventory>> ListByCategoryIdAsync(int IdCategory);
    Task<IEnumerable<Inventory>> ListByProductIdAsync(int IdProduct);
    Task<IEnumerable<Inventory>> ListByCategoryTitleAsync(string TitleCategory);
