d76fa14b7bf9a725d59dc102767ae78d91ea507c
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-10 14:38:58 -0500

    Added ClassRepository

---
+++ SSYS.API/SCM/Domain/Repositories/IInventoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using SSYS.API.SCM.Domain.Models;
+
+public interface IInventoryRepository
+{
+    Task<IEnumerable<Inventory>> ListAsync();
+    Task AddAsync(Inventory inventory);
+    Task<Inventory> FindByIdAsync(int id);
+    Task<Inventory> FindByCategoryIdAsync(int id);
+    Task<Inventory> FindByProductIdAsync(int id);
+    Task<Inventory> FindByCategoryTitleAsync(string title);
+    Task<Inventory> FindByProductTitleAsync(string title);
+    void Update(Inventory inventory);
+    void Remove(Inventory inventory);
+}
