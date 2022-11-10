a6744a24f328ea3090d184ccde56bc1ff5011391
QuispeJerry <89112834+QuispeJerry@users.noreply.github.com>
2022-11-10 14:46:14 -0500

    Update Functions

--- SSYS.API/SCM/Domain/Repositories/IInventoryRepository.cs
+++ SSYS.API/SCM/Domain/Repositories/IInventoryRepository.cs
@@ -4,11 +4,11 @@ public interface IInventoryRepository
 {
     Task<IEnumerable<Inventory>> ListAsync();
     Task AddAsync(Inventory inventory);
-    Task<Inventory> FindByIdAsync(int id);
-    Task<Inventory> FindByCategoryIdAsync(int id);
-    Task<Inventory> FindByProductIdAsync(int id);
-    Task<Inventory> FindByCategoryTitleAsync(string title);
-    Task<Inventory> FindByProductTitleAsync(string title);
+    Task<IEnumerable<Inventory>> FindByIdAsync(int id);
+    Task<IEnumerable<Inventory>> FindByCategoryIdAsync(int id);
+    Task<IEnumerable<Inventory>> FindByProductIdAsync(int id);
+    Task<IEnumerable<Inventory>> FindByCategoryTitleAsync(string title);
+    Task<IEnumerable<Inventory>> FindByProductTitleAsync(string title);
     void Update(Inventory inventory);
     void Remove(Inventory inventory);
 }
