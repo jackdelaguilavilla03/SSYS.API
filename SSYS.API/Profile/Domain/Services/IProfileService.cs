77c857b405c3d055878ec7d6cd9a1aa3e7d26aa8
Breydi Ramos Terrones <67590524+XxProFetxX@users.noreply.github.com>
2022-11-13 11:29:09 -0500

    profile service added.

--- SSYS.API/Profile/Domain/Services/IProfileService.cs
+++ SSYS.API/Profile/Domain/Services/IProfileService.cs
@@ -1,6 +1,11 @@
-﻿namespace SSYS.API.Profile.Domain.Services;
+﻿using SSYS.API.Profile.Domain.Services.Communication;
+
+namespace SSYS.API.Profile.Domain.Services;
 
 public interface IProfileService
 {
-    
+    Task<IEnumerable<Model.Entities.Profile>> ListAsync(); 
+    Task<ProfileResponse> SaveAsync(Model.Entities.Profile profile);
+    Task<ProfileResponse> UpdateAsync(int id, Model.Entities.Profile profile);
+    Task<ProfileResponse> DeleteAsync(int id);
 }
