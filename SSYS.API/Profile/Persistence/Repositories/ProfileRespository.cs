eb0f1fb6d4a0f091b8610e8f1ad04bcae8ccb030
Breydi Ramos Terrones <67590524+XxProFetxX@users.noreply.github.com>
2022-11-13 11:32:25 -0500

    feat(profile): profile respository implemented.

---
+++ SSYS.API/Profile/Persistence/Repositories/ProfileRespository.cs
@@ -0,0 +1,37 @@
+﻿using SSYS.API.Profile.Domain.Repositories;
+using SSYS.API.Shared.Persistence.Contexts;
+using SSYS.API.Shared.Persistence.Repositories;
+
+namespace SSYS.API.Profile.Persistence.Repositories;
+
+public class ProfileRespository : BaseRepository, IProfileRepository
+{
+    public ProfileRespository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Domain.Model.Entities.Profile>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddAsync(Domain.Model.Entities.Profile profile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Domain.Model.Entities.Profile> FindByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Domain.Model.Entities.Profile profile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Domain.Model.Entities.Profile profile)
+    {
+        throw new NotImplementedException();
+    }
+}
