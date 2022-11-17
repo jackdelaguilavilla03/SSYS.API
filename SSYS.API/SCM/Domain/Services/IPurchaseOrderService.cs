93daba57155f33c348120bc318e2b1ff4c4d4874
GiulianoMendez <u201824528@upc.edu.pe>
2022-11-17 14:15:06 -0500

    Update IPurchaseOrderService.cs

--- SSYS.API/SCM/Domain/Services/IPurchaseOrderService.cs
+++ SSYS.API/SCM/Domain/Services/IPurchaseOrderService.cs
@@ -1,6 +1,13 @@
-﻿namespace SSYS.API.SCM.Domain.Services;
+﻿using SSYS.API.SCM.Domain.Models;
+using SSYS.API.SCM.Domain.Services.Comunication;
+
+namespace SSYS.API.SCM.Domain.Services;
 
 public interface IPurchaseOrder
 {
-    
+    Task<IEnumerable<PurchaseOrder>> ListAsync();
+    Task<IEnumerable<PurchaseOrder>> ListByPurchaseOrderIdAsync(int id);
+    Task<PurchaseOrderResponse> SaveAsync(PurchaseOrder purchaseOrder);
+    Task<PurchaseOrderResponse> UpdateAsync(int purchaseOrderId, PurchaseOrder purchaseOrder);
+    Task<PurchaseOrderResponse> DeleteAsync(int purchaseOrderId);
 }
